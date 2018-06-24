bbf4793cfc8037c1b6482be8092215b5f7a8db2f
Laksh Advani <laksh.advani@gmail.com>
2018-06-24 14:35:37 -0700

    q1
    q1

---
+++ arraysum.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class arraysum {
+
+    static int simpleArraySum(int[] ar) {
+    int sum = 0;
+    for(int i:ar){
+    sum = sum+i;
+    }
+   System.out.println(sum);
+    return sum;
+    }
+
+    
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+
+        int[] arc = {
+        100,200,300
+        };
+        arraysum s = new arraysum();
+        int ss = s.simpleArraySum(arc);
+    }
+}
