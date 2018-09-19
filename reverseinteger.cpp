890f3a564b1df45a7a888dae4c798a42cf969ddc
Laksh Advani <laksh.advani@gmail.com>
2018-09-19 13:24:23 -0600

    Update reverseinteger.cpp

--- reverseinteger.cpp
+++ reverseinteger.cpp
@@ -8,6 +8,7 @@ public:
             remainder = x%10;
             reversedno = reversedno*10+remainder;
             x/=10;
+            cout << reversedno;
         }
         return reversedno;
     
@@ -26,5 +27,16 @@ reversedno = 0*10+remainder
 x = x/10
 }
 
-Essentially we need to 
+Example
+
+123
+!
+revno = 0
+remainder = None
+!!
+remiander = 3
+reversedno = 0*10+3 = 3
+x = 12
+
+
 */
