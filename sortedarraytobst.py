ec52aecbae0e67e57791b0c88b4f57068c1aeb0f
Laksh Advani <laksh.advani@gmail.com>
2018-09-19 23:24:48 -0600

    Create sortedarraytobst.py

---
+++ sortedarraytobst.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+        l,r = 0,len(nums)-1
+        mid = (l+(r-l))//2
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid+1:])
+        return node
+
+        
+'''
+DFS
+'''
+
